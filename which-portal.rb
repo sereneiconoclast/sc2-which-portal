1e2126e9072a127803e3bf72eb198fab563ebe35
Greg Meyers <greg.meyers.1138@gmail.com>
2025-12-25 08:03:27 -0800

    Some refactoring and tweaks to output

--- which-portal.rb
+++ which-portal.rb
@@ -4,23 +4,23 @@
 #   which-portal.rb from 175.3/145.4 to 468.1/091.6
 #   which-portal.rb from "175.3 / 145.4" to "468.1 / 091.6"
 
-# Portal data: name => [destination_x, destination_y]
+# Quasi-space address => [destination_x, destination_y]
 PORTALS = {
-  'Q521/514' => [11.2, 940.9],
-  'Q544/533' => [36.8, 633.0],
-  'Q530/528' => [775.2, 890.6],
-  'Q520/540' => [584.7, 621.1],
-  'Q488/538' => [973.5, 315.3],
-  'Q466/514' => [230.2, 398.8],
-  'Q448/504' => [565.8, 971.2],
-  'Q476/496' => [611.7, 413.1],
-  'Q458/492' => [860.7, 15.1],
-  'Q492/492' => [5.0, 164.7],
-  'Q468/464' => [921.1, 610.4],
-  'Q476/458' => [409.1, 774.8],
-  'Q502/460' => [318.4, 490.6],
-  'Q506/474' => [191.0, 92.6],
-  'Q516/466' => [567.3, 120.7]
+  'Q521 / 514' => [ 11.2, 940.9],
+  'Q544 / 533' => [ 36.8, 633.0],
+  'Q530 / 528' => [775.2, 890.6],
+  'Q520 / 540' => [584.7, 621.1],
+  'Q488 / 538' => [973.5, 315.3],
+  'Q466 / 514' => [230.2, 398.8],
+  'Q448 / 504' => [565.8, 971.2],
+  'Q476 / 496' => [611.7, 413.1],
+  'Q458 / 492' => [860.7,  15.1],
+  'Q492 / 492' => [  5.0, 164.7],
+  'Q468 / 464' => [921.1, 610.4],
+  'Q476 / 458' => [409.1, 774.8],
+  'Q502 / 460' => [318.4, 490.6],
+  'Q506 / 474' => [191.0,  92.6],
+  'Q516 / 466' => [567.3, 120.7]
 }.freeze
 
 PORTAL_COST = 10.0
@@ -62,7 +62,6 @@ target_x, target_y = parse_coordinates(ARGV[3])
 # Calculate direct travel cost
 direct_distance = distance(start_x, start_y, target_x, target_y)
 direct_fuel = round_up_to_tenth(direct_distance / 10.0)
-direct_total = direct_fuel
 
 # Find the portal with the closest destination to the target
 closest_portal = nil
@@ -78,18 +77,20 @@ end
 
 # Calculate portal travel fuel costs
 portal_travel_fuel = round_up_to_tenth(closest_distance / 10.0)
-portal_total = PORTAL_COST + portal_travel_fuel
+hyperspace_fuel = PORTAL_COST + portal_travel_fuel
 
 # Choose the cheaper option
-if direct_total <= portal_total
+if direct_fuel <= hyperspace_fuel
   # Direct travel is cheaper or equal
-  puts "Travel #{format('%.1f', direct_distance)} directly to #{format_coordinate(target_x)} / #{format_coordinate(target_y)} (cost: #{format('%.1f', direct_fuel)} units of fuel)"
-  puts "Total fuel cost: #{format('%.1f', direct_total)} units of fuel"
+  hyperspace_fuel = direct_fuel
+  portal_cost = 0.0
+  puts "Travel #{format('%.1f', direct_distance)} directly through hyperspace to #{format_coordinate(target_x)} / #{format_coordinate(target_y)} (cost: #{format('%.1f', direct_fuel)} units of fuel)"
 else
   # Portal travel is cheaper
-  puts "Use portal #{closest_portal[:name]} (cost: #{format('%.1f', PORTAL_COST)} units of fuel)"
-  puts "Emerge at #{format_coordinate(closest_portal[:dest_x])} / #{format_coordinate(closest_portal[:dest_y])}"
+  portal_cost = PORTAL_COST
+  puts "Use quasi-space portal at #{closest_portal[:name]} (cost: #{format('%.1f', portal_cost)} units of fuel to use portal spawner)"
+  puts "Emerge in hyperspace at #{format_coordinate(closest_portal[:dest_x])} / #{format_coordinate(closest_portal[:dest_y])}"
   puts "Travel #{format('%.1f', closest_distance)} to #{format_coordinate(target_x)} / #{format_coordinate(target_y)} (cost: #{format('%.1f', portal_travel_fuel)} units of fuel)"
-  puts "Total fuel cost: #{format('%.1f', portal_total)} units of fuel"
 end
+puts "Total fuel cost: #{format('%.1f', hyperspace_fuel)} units of fuel"
 
